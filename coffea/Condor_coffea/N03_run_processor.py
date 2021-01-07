ad97262fa483fd2c42f1a2ea4e70a2ed19022d3b
ico1036 <jiwoong.kim@cern.ch>
2021-01-07 23:08:19 +0900

    processor with AK1

--- coffea/Condor_coffea/N03_run_processor.py
+++ coffea/Condor_coffea/N03_run_processor.py
@@ -5,21 +5,22 @@ import time
 from coffea import processor, hist
 from coffea.util import load, save
 import json
-start = time.time()
 import glob 
 import os
 import argparse
+import numpy as np
+start = time.time()
 
 
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--nWorker', type=int,
-            help=" --nWorker 2", default=8)
+			help=" --nWorker 2", default=8)
 parser.add_argument('--metadata', type=str,
-            help="--metadata xxx.json")
+			help="--metadata xxx.json")
 parser.add_argument('--dataset', type=str,
-            help="--dataset ex) Egamma_Run2018A_280000")
+			help="--dataset ex) Egamma_Run2018A_280000")
 
 
 args = parser.parse_args()
@@ -29,46 +30,62 @@ args = parser.parse_args()
 # ---> Class MuZPeak
 class MyZPeak(processor.ProcessorABC):
 
-    # -- Initializer
-    def __init__(self):
-        self._histo = hist.Hist(
-            "Events",
-            hist.Cat("dataset","Dataset"),
-            hist.Bin("mass", "Z mass", 60, 60, 120)
-        )
-    # -- Accumulator: accumulate histograms
-    @property
-    def accumulator(self):
-        return self._histo
+	# -- Initializer
+	def __init__(self):
+		self._histo = hist.Hist(
+			"Events",
+			hist.Cat("dataset","Dataset"),
+			hist.Bin("mass", "Z mass", 60, 60, 120)
+		)
+	# -- Accumulator: accumulate histograms
+	@property
+	def accumulator(self):
+		return self._histo
 
-    # -- Main function : Process events
-    def process(self, events):
+	# -- Main function : Process events
+	def process(self, events):
 
-        # Initialize accumulator
-        out = self.accumulator.identity()
+		# Initialize accumulator
+		out = self.accumulator.identity()
 
-        # Event selection: opposite charged same flavor
-        mmevents = events[
-            (ak.num(events.Muon) == 2)
-            & (ak.sum(events.Muon.charge, axis=1) == 0 )
-        ]
+		# Event selection: opposite charged same flavor
 
-        # Select Muon and do Lorentz sum
-        zmm = mmevents.Muon[:,0] + mmevents.Muon[:,1]
+		Electron = events.Electron
+		Electron_mask = (Electron.pt >20) & (np.abs(Electron.eta) < 2.5) & (Electron.cutBased > 1) 
+		Ele_channel_mask = ak.num(Electron[Electron_mask]) > 1
+		Ele_channel_events = events[Ele_channel_mask]
+		Ele = Ele_channel_events.Electron
 
-        out.fill(
-            dataset = events.metadata["dataset"],
-            mass = zmm.mass
-        )
+		# All possible pairs of Electron in each event
+		ele_pairs = ak.combinations(Ele,2,axis=1)
+		
+		# TLorentz vector sum of ele_pairs
+		ele_left, ele_right = ak.unzip(ele_pairs)
+		diele = ele_left + ele_right
+		
+		
+		diffsign_diele =  diele[diele.charge==0]
+		
+		leading_diffsign_diele = diffsign_diele[ak.argmax(diffsign_diele.pt,axis=1,keepdims=True)]
 
-        return out
+		
+		#Mee = ak.flatten(leading_diffsign_diele.mass) # This makes type error ( primitive expected but ?float given )
+		Mee = ak.to_numpy(leading_diffsign_diele.mass)
+		Mee = Mee.flatten()
 
-    # -- Finally! return accumulator
-    def postprocess(self,accumulator):
-        return accumulator
+
+		out.fill(
+			dataset = events.metadata["dataset"],
+			mass = Mee
+		)
+
+		return out
+
+	# -- Finally! return accumulator
+	def postprocess(self,accumulator):
+		return accumulator
 # <---- Class MyZPeak
 
-#samples = {
 
 
 
@@ -93,13 +110,13 @@ result = processor.run_uproot_job(
 	"Events", # Tree name
 	MyZPeak(), # Class
 	executor=processor.futures_executor,
-    executor_args={"schema": NanoAODSchema, "workers": N_node},
-    #executor_args={'nano':True, "workers": N_node},
-    #maxchunks=4,
+	executor_args={"schema": NanoAODSchema, "workers": N_node},
+	#executor_args={'nano':True, "workers": N_node},
+	#maxchunks=4,
 )
 
 
-# Draw hist
+# Draw hist -- If you want to drwa hist
 #import matplotlib.pyplot as plt
 #hist.plot1d(result)
 #plt.savefig("Zmumu.png")
@@ -109,10 +126,9 @@ outname = data_sample + '.futures'
 save(result,outname)
 
 
-
-
 elapsed_time = time.time() - start
 print("Time: ",elapsed_time)
 ## <-------------------------
+
 
 
--- coffea/Processor/readme.md
+++
@@ -1,6 +0,0 @@
-### On progress..  
-
-processor.py is based on Awkward0 but not work  
- - Try this using awkward1 (Jiwoong, Bongho... developing)  
- - Try this using awkward0 (Next weak plan)   
-
